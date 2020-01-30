ff04da988e6ca30708af54812626f361cd53d04f
Luke <luke@MacBook-Air.local>
2020-01-29 18:17:49 -0600

    reads user input and updates correct lettters

--- Hangman.java
+++ Hangman.java
@@ -16,7 +16,7 @@ public class Hangman {
         // creating File instance to reference text file
         File text = new File("wordlist.txt");
         Scanner reader;
-        String choice;
+        String choice = "";
         // get chosen word from file
         try {
             int count = 0;
@@ -32,12 +32,78 @@ public class Hangman {
             // display error
             System.out.println("File not Found error");
             e.printStackTrace();
-        } 
-        
+        }
+
         // scanner for user input
         final Scanner input = new Scanner(System.in); // scanner for user input
+        int length = choice.length();
+        char[] word = choice.toCharArray();
+        // for(int i = 0; i < length; i++){
+        // System.out.println(word[i]);
+        // }
+        char hiddenWord[] = new char[length];
+        for (int i = 0; i < length; i++) {
+            hiddenWord[i] = '*';
+            // System.out.print(hiddenWord[i]);
+        }
+        int count = 0;
+        boolean solved = false;
+        while (!solved) {
+            printWord(hiddenWord);
+            System.out.print("Enter guess...");
+            if (input.hasNext()) {
+                String guess = input.next(); // get input as string
+                char letter = guess.charAt(0); // convert input to char
+                System.out.println("You guessed " + letter);
+                // checkGuess(hiddenWord,word,letter);
+                // check guess
+                boolean match = false;
+                for (int i = 0; i < hiddenWord.length; i++) {
+                    if (word[i] == letter) { // search for guess in word
+                        match = true;
+                        count++; //increment correct count
+                        hiddenWord[i] = letter; // update hidden word with correct guess
+                    }
+                }
+                //System.out.println("Count= " + count);
+                if(match){
+                    System.out.println("Correct!");
+                }
+                else{
+                    System.out.println("Wrong!");
+                }
+
+            }
+            if(count == word.length){
+                solved = true;
+            }
+        }
+        printWord(hiddenWord);
+        System.out.println("Solved!");
+        System.out.println();
 
         input.close();
+    }
+
+    // displays hidden word to user
+    //
+    public static void printWord(char[] hiddenWord) {
+        for (int i = 0; i < hiddenWord.length; i++) {
+            System.out.print(hiddenWord[i]);
+        }
+        System.out.println();
+        System.out.println(); // spacing for easy reading
+    }
+
+    public static boolean checkGuess(char[] hiddenWord, char[] word, char c) {
+        boolean match = false;
+        for (int i = 0; i < hiddenWord.length; i++) {
+            if (word[i] == c) {
+                match = true;
+                hiddenWord[i] = c;
+            }
+        }
+        return match;
     }
 
 }
